887a91862ef5775bf6228e7bc9ae40a001f1797c
CerkezDeni <deni.cerkez@gmail.com>
2022-06-14 18:24:31 +0200

    PROJEKTNI_KRAJ
    Spremno za prezentiranje

--- Super-Projekt.cpp
+++ Super-Projekt.cpp
@@ -8,6 +8,7 @@
 
 
 class kvadrat {
+
 private:
 	int x = 0;
 	int y = 0;
@@ -44,6 +45,7 @@ public:
 	{
 		std::cout << " " << pijun << " ";
 	}
+
 	char getX()
 	{
 		switch (x)
@@ -68,14 +70,17 @@ public:
 			exit(1);
 		}
 	}
+
 	char getY()
 	{
 		return y + 1 + '0';
 	}
+
 	char getPijun()
 	{
 		return pijun;
 	}
+
 	void pojediPijuna()
 	{
 		pijun = ' ';
@@ -87,9 +92,12 @@ void promjenaIgraca(char& igrac, bool& dodatnaMeta, bool& selectSet, bool& targe
 void refreshPloce(kvadrat polje[8][8], char trenutacniIgrac);
 void dodatanPotez(int& selectX, int& selectY, int& targetX, int& targetY);
 
+
 ///////////////////////////////////////////////////
 int main()
 {
+
+
 	kvadrat polje[8][8];
 	int selectX, selectY, targetX, targetY;
 
@@ -101,6 +109,7 @@ int main()
 
 	if (choice == "Y" || choice == "y") //ucitavanje
 	{
+
 		system("cls");
 		std::cout << "Ime spremljene datoteke? ";
 		std::cin >> FILENAME;
@@ -115,6 +124,7 @@ int main()
 		file.close();
 		std::cout << "Ucitana igra! ";
 		std::cin.ignore();
+
 	}
 
 
@@ -147,8 +157,10 @@ int main()
 		
 		do
 		{
+
 			if (selectSet == false)
 			{   
+
 				nazad:
 				std::cout << "\nIzaberi pijuna (npr. a3) ili upisi \"sv\" da spremis igru: ";
 				getline(std::cin, odabir);
@@ -159,7 +171,6 @@ int main()
 				if ((red_from == 's' && stupac_from == 'v') || (red_from == 'S' && stupac_from == 'v') || (red_from == 'S' && stupac_from == 'V')) //save-anje igre
 				{
 					std::cout << "Upisi ime za spremanje datoteke: ";
-					std::cout << "Spremanje igre!" << std::endl;
 					std::cin >> FILENAME;
 					file.open(FILENAME, std::ios::binary | std::ios::out);
 					file.write((char*)&polje, sizeof(polje));
@@ -170,6 +181,7 @@ int main()
 
 				else if ((odabir[1] > '0' && odabir[1] <= '8') && (odabir[0] >= 'a' && odabir[0] <= 'h')) //provjerava da li je moguce otici na polje koje napadas
 				{
+
 					selectX = odabir[0] - 96 - 1;
 					selectY = odabir[1] - 48 - 1;
 
@@ -184,16 +196,19 @@ int main()
 						polje[selectX][selectY].ispisPolja();
 					}
 					
+
 				}
 				else
 				{
 					std::cout << "Krivi upis!.\n";
 				}
 			}
+
 			else
 			{
 				break;
 			}
+
 		} while (true);
 
 		
@@ -201,6 +216,7 @@ int main()
 		{
 			std::cout << "\nIzaberi polje na koje se pomices ili \"back\" da vratis potez : "; 
 			getline(std::cin, target);
+
 			if (target == "back")
 			{
 				goto nazad;
@@ -208,20 +224,24 @@ int main()
 			
 			if ((target[1] > '0' && target[1] <= '8') && (target[0] >= 'a' && target[0] <= 'h'))
 			{
+
 				std::cout << "Valid\n";
 				targetX = target[0] - 96 - 1;
 				targetY = target[1] - 48 - 1;
 
 				if ((targetX + targetY) % 2 == 0 && polje[targetX][targetY].getPijun() == ' ')////provjerava dali je polje na koje se pomices prazno
 				{
+
 					if ((targetX - selectX == 1 || targetX - selectX == -1) && (targetY - selectY == 1 || targetY - selectY == -1))//provjerava dali se hocemo kretati jednom
 					{
 						std::swap(polje[selectX][selectY], polje[targetX][targetY]);
 						targetSet = true;
 						break;
 					}
+
 					else if ((targetX - selectX == 2 || targetX - selectX == -2) && (targetY - selectY == 2 || targetY - selectY == -2))//provjerava dali se hocemo kretati dvaput
 					{
+
 						if (polje[(selectX + targetX) / 2][(selectY + targetY) / 2].getPijun() != ' ' && polje[(selectX + targetX) / 2][(selectY + targetY) / 2].getPijun() != trenutacniPotez)//provjerava da li ima pijuna
 						{
 							polje[(selectX + targetX) / 2][(selectY + targetY) / 2].pojediPijuna();
@@ -238,6 +258,7 @@ int main()
 									break;
 								}
 							}
+
 							if ((targetX - 2 >= 0 && targetY - 2 >= 0) && polje[targetX - 1][targetY - 1].getPijun() != ' ' && polje[targetX - 1][targetY - 1].getPijun() != trenutacniPotez)
 							{
 								if (polje[targetX - 2][targetY - 2].getPijun() == ' ')
@@ -248,6 +269,7 @@ int main()
 									break;
 								}
 							}
+
 							if ((targetX - 2 >= 0 && targetY + 2 < 8) && polje[targetX - 1][targetY + 1].getPijun() != ' ' && polje[targetX - 1][targetY + 1].getPijun() != trenutacniPotez)
 							{
 								if (polje[targetX - 2][targetY + 2].getPijun() == ' ')
@@ -258,6 +280,7 @@ int main()
 									break;
 								}
 							}
+
 							if ((targetX + 2 < 8 && targetY - 2 >= 0) && polje[targetX + 1][targetY - 1].getPijun() != ' ' && polje[targetX + 1][targetY - 1].getPijun() != trenutacniPotez)
 							{
 								if (polje[targetX + 2][targetY - 2].getPijun() == ' ')
@@ -270,37 +293,47 @@ int main()
 							}
 							break;
 						}
+
 						else
 						{
 							std::cout << "Krivi potez.\n";
 						}
 					}
+
 					else
 					{
 						std::cout << "Krivi potez.\n";
 					}
 				}
+
 				else
 				{
 					std::cout << "Krivi potez.\n";
 				}
 			}
+
 			else
 			{
 				std::cout << "Krivi potez.\n";
 			}
 		} while (true);
+
 		promjenaIgraca(trenutacniPotez, dodatnaMeta, selectSet, targetSet);
+
 	} while (true);
+
 	system("pause>0");
+
 }
 
 
 ////
 void promjenaIgraca(char& igrac, bool& novaMeta, bool& selectSet, bool& targetSet)  //dvostruki napad
 {
+
 	if (novaMeta == true)
 	{
+
 		std::cout << "\nNovi napad? (y/n): ";
 		char yn;
 		std::cin >> yn;
@@ -312,14 +345,17 @@ void promjenaIgraca(char& igrac, bool& novaMeta, bool& selectSet, bool& targetSe
 			novaMeta = false;
 			targetSet = false;
 			return;
+
 		default:
 			novaMeta = false;
 			break;
 		}
+
 	}
 
 	switch (igrac)
 	{
+
 	case 'B':
 		igrac = 'C';
 		break;
@@ -328,6 +364,7 @@ void promjenaIgraca(char& igrac, bool& novaMeta, bool& selectSet, bool& targetSe
 		break;
 	default:
 		exit(2);
+
 	}
 
 	targetSet = false;
@@ -336,6 +373,7 @@ void promjenaIgraca(char& igrac, bool& novaMeta, bool& selectSet, bool& targetSe
 
 void refreshPloce(kvadrat polje[8][8], char trenutacniIgrac)
 {
+
 	system("cls");
 
 	for (int visina = 8 * 4; visina >= 0; visina--)  // ispis bas ploce
@@ -358,6 +396,7 @@ void refreshPloce(kvadrat polje[8][8], char trenutacniIgrac)
 				}
 			}
 		}
+
 		else if (visina % 4 == 2)
 		{
 			for (int sirina = 0; sirina <= 8 * 4; sirina++)
@@ -380,6 +419,7 @@ void refreshPloce(kvadrat polje[8][8], char trenutacniIgrac)
 				}
 			}
 		}
+
 		else
 		{
 			for (int sirina = 0; sirina <= 8 * 4; sirina++)
@@ -395,14 +435,18 @@ void refreshPloce(kvadrat polje[8][8], char trenutacniIgrac)
 			}
 		}
 
+
 		std::cout << "\n";
 	}
+
 
 	std::cout << "\n";
 	std::cout << "\nTrenutacni igrac: " << trenutacniIgrac << "\n";
 
 
 }
+
+
 
 void dodatanPotez(int& selectX, int& selectY, int& targetX, int& targetY)
 {
